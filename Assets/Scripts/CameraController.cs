8680ddd931ce3de7eeb9da3acb93b4a50550e5c5
Tiger <thisisstreamliner@gmail.com>
2020-06-17 16:22:09 +0300

    Making the camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
