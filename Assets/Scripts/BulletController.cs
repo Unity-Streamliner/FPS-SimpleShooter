f7d080d737a0cd17321733b229222257553605bd
thisisstreamliner <thisisstreamliner@gmail.com>
2020-07-29 16:51:50 +0300

    Added Moving The Projectile

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rigidbody;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = transform.forward * moveSpeed;
+    }
+}
