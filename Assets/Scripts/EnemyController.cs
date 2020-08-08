2dcea38b5c9a02a7f60e40a1f24034e12bcff47f
thisisstreamliner <thisisstreamliner@gmail.com>
2020-08-08 13:17:24 +0300

    More Enemy Navigation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,12 +9,16 @@ public class EnemyController : MonoBehaviour
     private bool chasing;
     public float distanceToChase = 10f;
     public float distanceToLose = 15f;
+    public float distanceToStop = 2f;
     private Vector3 targetPoint;
     public NavMeshAgent navMeshAgent;
+    private Vector3 initialPosition;
+    public float keepChasingTime = 5f;
+    private float chaseCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -28,12 +32,28 @@ public class EnemyController : MonoBehaviour
             {
                 chasing = true;
             }
+
+            if (chaseCounter > 0)
+            {
+                chaseCounter -= Time.deltaTime;
+                if (chaseCounter <= 0)
+                {
+                    navMeshAgent.SetDestination(initialPosition);
+                }
+            }
         } else 
         {
-            navMeshAgent.destination = targetPoint;
+            if (Vector3.Distance(transform.position, targetPoint) > 2f)
+            {
+                navMeshAgent.destination = targetPoint;
+            } else
+            {
+                navMeshAgent.destination = transform.position;
+            }
             if (Vector3.Distance(transform.position, targetPoint) > distanceToLose)
             {
                 chasing = false;
+                chaseCounter = keepChasingTime;
             } 
         }
 
